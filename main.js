72225847ce93fb17428bfb7699b27e102e438421
yanna <barlisan.al@obmontessori.edu.ph>
2026-04-13 17:03:02 +0800

    Update login credentials in loginUser function

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ function loginUser() {
     let username = document.getElementById("usernameInput").value;
     let password = document.getElementById("passwordInput").value;
 
-    if (username === "follow_yishahaha_onletterboxd" && password === "12345") {
+    if (username === "digitalynna_" && password === "67890") {
         window.alert("Login successful! Welcome, " + username + "!");
 
     } else {
